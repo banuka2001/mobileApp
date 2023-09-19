7edaa09ab01e7712c6bc6b86ec9de7e0b20be819
banuka2001 <91893782+banuka2001@users.noreply.github.com>
2023-09-19 18:56:35 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const addBtn = document.getElementById("add-button")
+const inputEl = document.getElementById("input-field")
+
+addBtn.addEventListener("click",function(){
+    console.log(inputEl.value)     
+})
